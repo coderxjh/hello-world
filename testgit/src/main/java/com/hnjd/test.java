4bc475735b4dfa7bbaa1cc291c8843fc1cc6b774
xiaojianghui <123456@qq.com>
2020-10-16 20:30:15 +0800

    创建分支

--- testgit/src/main/java/com/hnjd/test.java
+++ testgit/src/main/java/com/hnjd/test.java
@@ -9,5 +9,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("更新1");
+        System.out.println("创建分支");
     }
 }
